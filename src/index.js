3a51c0c5f6ed68a66fb54a65ad84b1b6c62a507e
bladi452 <cleto662@gmail.com>
2021-05-31 10:21:16 -0400

    Servidor basico

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
+    //Require
 const express = require ('express');
+
+
+    //Initialization
+const app = express()
+
+app.use(express.json());
+
+    //Settings
+
+app.set('port', process.env.PORT || 4000);
+
+    //Middlewares
+
+
+
+    //Global Variables
+
+
+
+    //Routes
+
+
+
+    //Public
+
+
+    //Starting the server
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port')
+})
 
