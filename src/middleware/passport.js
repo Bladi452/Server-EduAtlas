a8076e58e39819435acc5017314610f1b3374f8f
bladi452 <cleto662@gmail.com>
2021-07-01 11:21:20 -0400

    Passport culminado

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -7,17 +7,43 @@ import helpers from './helpers';
 
 const db = await connect()
 
-passport.use('local.singin', new LocalStrategy({
+passport.use('local.signin', new LocalStrategy({
     usernameField: 'usermame',
     passwordField: 'password',
-}, async (req, username, password, donde) =>{
+}, async (req, username, password, done) =>{
     const rows = await db.query('SELECT * FROM usuario WHERE matricula = ?', [username]);
 if(rows.length > 0){
     const user = rows[0];
     const validPassword = await helpers.matchPassword(passport, user.password)
-}
+    if (validPassword) {
+        done(null, user)
+    } else {
+        done(null, false)
+    }
+    }else{
+        return done(null, false)
+    }
+}));
 
-}
+passport.use('local.signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+},async (req, username, password, done) =>{
+    
+    const {fullname} = req.body;
+    let newUser = {
+        fullname,
+        username,
+        password
+    };
+    newUser.password = await helpers.encryptPassword(password);
 
+    const resultado =await db.query('INSERT INTO usuario SET ?', newUser);
+    newUser.id = resultado.insertid;
+    return done(null, newUser);
+}))
 
-))
+passport.deserializeUser(async (id, done)=>{
+    const rows = await db.query('SELECT * FROM usuario WHERE id = ?', [id])
+})
