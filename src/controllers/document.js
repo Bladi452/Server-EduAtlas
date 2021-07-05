ff810ff7ddcd4f5c265cd09a415634a97c32ea41
bladi452 <cleto662@gmail.com>
2021-07-05 17:54:54 -0400

    Documentos terminados
    Co-authored-by: daniperezdpj <daniperezdpj@users.noreply.github.com>

---
+++ src/controllers/document.js
@@ -0,0 +1,46 @@
+import {connect} from '../database'
+
+export const download = async(req, res) =>{
+    const db = await connect()
+    const pass = await db.query("SELECT * FROM documentos WHERE Id_documentos = ?",[req.params.id])
+
+    const file = __dirname + pass[0][0].UrlDocs
+    res.download(file)
+}
+
+
+export const uploadApp = async (req, res) =>{
+    let sampleFile;
+    let uploadPath;
+ 
+ 
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      res.status(400).send('No files were uploaded.');
+      return;
+    }
+  
+    console.log('req.files >>>', req.files); // eslint-disable-line
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    ruta = '/upload/' + sampleFile.name
+    uploadPath = __dirname + ruta;
+ 
+     // Use the mv() method to place the file somewhere on your server
+  
+    const db = await connect()
+  
+    sampleFile.mv(uploadPath, async function (err) {
+       await db.query("INSERT INTO documentos (UrlDocs, Estado ,Id_Usuario) VALUES (?,?,?)",[
+          ruta,
+          "null",
+          "1"
+       ])
+       
+       if (err) {
+        return res.status(500).send(err);
+      }
+ 
+      res.send('File uploaded to ' + uploadPath);
+    });
+  }
--- src/routes/document.js
+++ src/routes/document.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import {uploadApp, download} from '../controllers/document'
+
 const router = Router();
 
-router.get('/document/:id');
+router.get('/document/:id', download);
 
-router.post('/document');
+router.post('/document', uploadApp);
 
 export default router
