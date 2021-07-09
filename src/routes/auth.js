2c62462960504efb85e3e40b254fec02fcf74c7c
bladi452 <cleto662@gmail.com>
2021-07-09 11:52:32 -0400

    Servidor arreglado

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,6 @@
 import {Router} from 'express'
 import {validar, registrar, isAuth} from "../controllers/auth"
 const router = Router();
-
 router.post('/auth/regis',registrar)
 router.post('/auth/login',validar)
 router.get('/auth/private', isAuth)
--- src/routes/school.js
+++ src/routes/school.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {addReq, getSchool, idSelect} from "../controllers/school"
+import {isAuth} from "../controllers/auth"
 
 const router = Router();
 
