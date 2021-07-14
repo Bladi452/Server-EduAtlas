9b2b3a1570aedde712796de61bb7917f67b8a6f6
bladi452 <cleto662@gmail.com>
2021-07-14 17:26:04 -0400

    Navegacion terminada

--- src/controllers/navegacion.js
+++ src/controllers/navegacion.js
@@ -8,7 +8,7 @@ export const navegacion = async (req, res) =>{
     res.json(user[0])
 }
 
-export const navegacion = async (req, res) =>{
+export const navegacionGetSol = async (req, res) =>{
         const db = await connect()
         const solicitud = await db.query("SELECT * FROM solicitud WHERE Matricula = ?", [req.params.id])
         res.json(solicitud[0])
--- src/routes/navegacion.js
+++ src/routes/navegacion.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import {navegacion} from '../controllers/navegacion'
+import {navegacion,navegacionGetSol} from '../controllers/navegacion'
 
 const router = Router();
 
 router.get('/navegacion/:id', navegacion);
+
+router.get('/navegacionSel/:id', navegacionGetSol);
 
 export default router
