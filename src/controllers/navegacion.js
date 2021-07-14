5eaecc891f5930c8e37bc861fb85848e566df73f
bladi452 <cleto662@gmail.com>
2021-07-14 16:52:33 -0400

    Navegacion terminada

--- src/controllers/navegacion.js
+++ src/controllers/navegacion.js
@@ -8,9 +8,10 @@ export const navegacion = async (req, res) =>{
     const user = await db.query("SELECT * FROM usuario WHERE Matricula = ?",[req.params.id])
   
     res.json(user[0])
-    }
+}
 
-    export const navegacion = async (req, res) =>{
+export const navegacion = async (req, res) =>{
         const db = await connect()
-        const 
-    }
+        const solicitud = await db.query("SELECT * FROM solicitud WHERE Matricula = ?", [req.params.id])
+        res.json(solicitud[0])
+}
