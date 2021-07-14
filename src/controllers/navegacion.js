147467ce428f96318c985ea01d6e2697d8e94c87
bladi452 <cleto662@gmail.com>
2021-07-14 16:53:32 -0400

    Navegacion terminada completa

--- src/controllers/navegacion.js
+++ src/controllers/navegacion.js
@@ -2,8 +2,6 @@ import {connect} from '../database'
 
 export const navegacion = async (req, res) =>{
     const db = await connect()
-    
-    const Matricula = req.body.Matricula;
 
     const user = await db.query("SELECT * FROM usuario WHERE Matricula = ?",[req.params.id])
   
